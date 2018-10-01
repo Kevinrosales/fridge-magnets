61d72f4b7f2261f12c62890b498e4b6654bda1bc
Charles Clemens <cclemensjr@gmail.com>
2018-10-01 16:48:20 -0700

    Set object properties

--- js/app.js
+++ js/app.js
@@ -2,7 +2,10 @@
 
 var magnets = ["A","a","B","b","C","c","D","d","E","e","F","f","G","g","H","h","I","i","J","j","K","k","L","l","M","m","N","n","O","o","P","p","Q","q","R","r","S","s","T","t","U","u","V","v","W","w","X","x","Y","y","Z","z"];
 
-function Magnet(magnetName, positionX, positionY)
+function Magnet(magnet, x, y)
 {
+  this.magnetName = magnet;
+  this.positionX = x;
+  this.positionY = y;
 
 }
