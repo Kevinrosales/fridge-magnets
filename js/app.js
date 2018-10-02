360d71b3dea2ed5a0f66045aa2cef3dafb9992e0
Charles Clemens <cclemensjr@gmail.com>
2018-10-02 11:02:36 -0700

    Updated name for letters array

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var magnets = ['A','a','B','b','C','c','D','d','E','e','F','f','G','g','H','h','I','i','J','j','K','k','L','l','M','m','N','n','O','o','P','p','Q','q','R','r','S','s','T','t','U','u','V','v','W','w','X','x','Y','y','Z','z'];
+var magnetLetters = ['A','a','B','b','C','c','D','d','E','e','F','f','G','g','H','h','I','i','J','j','K','k','L','l','M','m','N','n','O','o','P','p','Q','q','R','r','S','s','T','t','U','u','V','v','W','w','X','x','Y','y','Z','z'];
 
 function Magnet(magnet, x, y) {
   this.magnetName = magnet;
@@ -24,6 +24,11 @@ function checkLocalStorage()
     console.log('Found it in storage');
     Magnet.allMagnets = magnets;
   }
+}
+
+function createMagnets()
+{
+  // Create a for loop set to the length of the magnets array
 }
 
 function rando(min, max)
