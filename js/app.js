3600bf8a1eccd0a23e329044f503f8537f6e190d
Charles Clemens <cclemensjr@gmail.com>
2018-10-02 08:44:07 -0700

    Create function that checks local storage

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
 'use strict';
 
-var magnets = ["A","a","B","b","C","c","D","d","E","e","F","f","G","g","H","h","I","i","J","j","K","k","L","l","M","m","N","n","O","o","P","p","Q","q","R","r","S","s","T","t","U","u","V","v","W","w","X","x","Y","y","Z","z"];
+var magnets = ['A','a','B','b','C','c','D','d','E','e','F','f','G','g','H','h','I','i','J','j','K','k','L','l','M','m','N','n','O','o','P','p','Q','q','R','r','S','s','T','t','U','u','V','v','W','w','X','x','Y','y','Z','z'];
 
-function Magnet(magnet, x, y)
-{
+function Magnet(magnet, x, y) {
   this.magnetName = magnet;
   this.positionX = x;
   this.positionY = y;
@@ -13,3 +12,15 @@ function Magnet(magnet, x, y)
 
 Magnet.allMagnets = [];
 
+function checkLocalStorage()
+{
+  var localMagnets = localStorage.getItem('magnets');
+  var magnets = JSON.parse(localMagnets);
+
+  if (magnets && magnets.length) {
+    console.log('Found it in storage');
+    Magnet.allMagnets = magnets;
+  }
+}
+
+checkLocalStorage();
